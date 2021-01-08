d937733b43d2390227b764e3c173423cfb867aa6
Ward Peeters <ward@coding-tech.com>
2021-01-08 17:26:09 +0100

    feat(gatsby): Improve cssmodules performance

--- packages/gatsby/src/utils/webpack.config.js
+++ packages/gatsby/src/utils/webpack.config.js
@@ -527,7 +527,8 @@ module.exports = async (
 
   if (stage === `build-javascript`) {
     const componentsCount = store.getState().components.size
-    const isCssModule = module => module.type === `css/mini-extract`
+    const isCssModule = module =>
+      module.type === `css/mini-extract` && !module.issuer.usedExports
 
     const splitChunks = {
       chunks: `all`,
