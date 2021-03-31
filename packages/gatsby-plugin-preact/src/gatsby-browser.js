9367be7c2a7e4cf3f468ecaf588136658d1ca83e
Ward Peeters <ward@coding-tech.com>
2021-03-31 10:45:30 +0200

    feat(gatsby-plugin-preact): enable error-overlay

--- packages/gatsby-plugin-preact/src/gatsby-browser.js
+++ packages/gatsby-plugin-preact/src/gatsby-browser.js
@@ -1,5 +1,43 @@
+const singletonKey = `__webpack_hot_middleware_reporter__`
+
+function messageHandler(message) {
+  switch (message.type) {
+    case `ok`:
+    case `still-ok`:
+    case `warnings`: {
+      // TODO: Implement handling for warnings
+      handleCompileSuccess()
+      break
+    }
+    case `errors`: {
+      handleCompileErrors(message.data)
+      break
+    }
+    default: {
+      // Do nothing.
+    }
+  }
+}
+
 exports.onClientEntry = () => {
   if (process.env.NODE_ENV !== `production`) {
     require(`preact/debug`)
   }
+
+  const client =
+    window[singletonKey] || require(`webpack-hot-middleware/client`)
+
+  client.useCustomOverlay({
+    showProblems: function showProblems(type, data) {
+      const error = {
+        data: data,
+        type: type,
+      }
+
+      messageHandler(error)
+    },
+    clear: function clear() {
+      messageHandler({ type: `ok` })
+    },
+  })
 }
